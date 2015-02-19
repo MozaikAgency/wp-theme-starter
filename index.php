e8550a8e8aa351f08200dfc7e51c472269708889
Max GJ Panas <m@maxpanas.com>
2015-02-19 18:10:56 +0200

    All you need for a valid WordPress Theme

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Read up on the WP Template Hierarchy for
+ * when this file is used
+ *
+ * @author: Mozaik Ltd. <http://mozaik.com/>
+ */
+?>
+<?php get_header(); ?>
+
+
+
+<?php get_footer(); ?>
