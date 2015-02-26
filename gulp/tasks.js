bb389a1b84e7d64412459e3f5d655972db55d199
Max GJ Panas <m@maxpanas.com>
2015-02-26 12:04:46 +0200

    add description to `pumped` gulp utility

--- gulp/tasks.js
+++ gulp/tasks.js
@@ -1,14 +1,18 @@
 var gulp = require('gulp');
 
+var scssCompile = require('./recipes/scss-compile');
+var themeMove   = require('./recipes/theme-move');
+
+
 
 /**
  * Styles
  */
-gulp.task('scss-compile', [], require('./recipes/scss-compile'));
+gulp.task('scss-compile', [], scssCompile);
 
 
 /**
  * Theme
  */
-gulp.task('theme-move', [], require('./recipes/theme-move'));
+gulp.task('theme-move', [], themeMove);
 gulp.task('theme-build', ['theme-move', 'scss-compile']);
