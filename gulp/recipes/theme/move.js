6a1937707f0940d8fd9b59287e2cb38e74f7cebe
Max GJ Panas <m@maxpanas.com>
2015-03-03 18:55:56 +0200

    change wpStyles.js to wp.stylescss.js to be more descriptive and to match the general theme of the project

--- gulp/recipes/theme/move.js
+++ gulp/recipes/theme/move.js
@@ -5,7 +5,7 @@ var changed      = require('gulp-changed');
 var notify       = require('gulp-notify');
 var displayError = require('../../utils/displayError');
 var pumped       = require('../../utils/pumped');
-var wpStyles     = require('../../config/wpStyles');
+var stylescss    = require('../../config/wp.stylescss');
 var project      = require('../../../package.json');
 
 
@@ -20,7 +20,7 @@ module.exports = function () {
 		.pipe(plumber({ errorHandler: displayError }))
 		.pipe(add({
 			'.gitignore': '*',
-			'styles.css': wpStyles
+			'styles.css': stylescss
 		}))
 		.pipe(changed('../' + project.name, { hasChanged: changed.compareSha1Digest }))
 		.pipe(gulp.dest('../' + project.name))
