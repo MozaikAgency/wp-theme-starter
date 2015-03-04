b56e84830e55a8e5b7d95f378f618da157e0fc2d
Max G J Panas <m@maxpanas.com>
2015-03-04 04:46:04 +0200

    add sourcemaps to styles:compile

--- gulp/recipes/styles/compile.js
+++ gulp/recipes/styles/compile.js
@@ -1,21 +1,28 @@
+var _            = require('lodash');
 var gulp         = require('gulp');
 var plumber      = require('gulp-plumber');
+var gulpif       = require('gulp-if');
+var sourcemaps   = require('gulp-sourcemaps');
 var sass         = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var notify       = require('gulp-notify');
+var browserSync  = require('browser-sync');
 var displayError = require('../../utils/displayError');
 var pumped       = require('../../utils/pumped');
 var project      = require('../../../package.json');
 
 
+var devmode = false;
+
 /**
  * Compile SCSS to CSS
  *
  */
-module.exports = function () {
+module.exports = function (cb) {
 	return gulp.src(['assets/scss/**/*.scss', '!assets/scss/**/_*'])
-		.pipe(plumber({errorHandler: displayError}))
-		.pipe(sass({onError: console.log}))
+		.pipe(plumber({ errorHandler: displayError }))
+    .pipe(gulpif(devmode, sourcemaps.init()))
+		.pipe(sass({ errorToConsole: true }))
 		.pipe(autoprefixer(
 			'last 2 version'
 			/*'safari 5',
@@ -26,9 +33,11 @@ module.exports = function () {
 			 'ios 6',
 			 'android 4'*/
 		))
+    .pipe(gulpif(devmode, sourcemaps.write()))
 		.pipe(gulp.dest('../' + project.name + '/assets/css'))
+    .pipe(gulpif(devmode, browserSync.reload({ stream: true })))
 		.pipe(notify({
 			message: pumped('SCSS compiled.'),
 			onLast: true
-		}));
+    }));
 };
