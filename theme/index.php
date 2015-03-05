5e07583dab9ceaee395a778af3f48bc5cd84cba5
Max G J Panas <m@maxpanas.com>
2015-03-05 05:16:54 +0200

    add friendly greeting to index file, also helps to demonstrate that the theme is working

--- theme/index.php
+++ theme/index.php
@@ -9,5 +9,16 @@
 <?php get_header(); ?>
 
 
+<h1>Welcome!</h1>
+
+<h2>The Mozaik WordPress Theme Bootstrap</h2>
+
+<p>
+	Build tools included! :)
+</p>
+
+<p>
+	<strong>Happy Hacking!</strong>
+</p>
 
 <?php get_footer(); ?>
