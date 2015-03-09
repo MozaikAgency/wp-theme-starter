89d63a896e6366af50ed899287767d785fd8711d
Max GJ Panas <m@maxpanas.com>
2015-03-09 18:31:03 +0200

    remove stray console.log in scripts prod

--- gulp/recipes/scripts/prod.js
+++ gulp/recipes/scripts/prod.js
@@ -2,8 +2,8 @@ var gulp         = require('gulp');
 var plumber      = require('gulp-plumber');
 var named        = require('vinyl-named');
 var gulpWebpack  = require('gulp-webpack');
+var notify       = require('gulp-notify');
 var lodash       = require('lodash');
-var notifier     = require('node-notifier');
 var displayError = require('../../utils/displayError');
 var pumped       = require('../../utils/pumped');
 
@@ -17,31 +17,26 @@ var config       = require('../../config/scripts');
  * @returns {*}
  */
 module.exports = function () {
-	console.log(gulpWebpack.get());
 	return gulp.src(config.paths.src)
 		.pipe(plumber({ errorHandler: displayError }))
 
 		.pipe(named()) // vinyl-named is used to allow for
 									 // multiple entry files
-		.pipe(gulpWebpack(
-			lodash.merge(config.options.webpack, {
-				plugins: [
-					// gulp-webpack exposes the webpack library
-					// it uses through the webpack property
-					new gulpWebpack.webpack.optimize.DedupePlugin(),
-					new gulpWebpack.webpack.optimize.OccurenceOrderPlugin(true),
-					new gulpWebpack.webpack.optimize.UglifyJsPlugin({
-						minimize: true
-					})
-				]
-			}), null, function () {
-				notifier.notify({
-					title:   'Webpack',
-					message: pumped('JS Packaged & Minified!'),
-					onLast:  true
-				});
-   	 	})
-		)
+		.pipe(gulpWebpack( lodash.merge(config.options.webpack, {
+			plugins: [
+				// gulp-webpack exposes the webpack library
+				// it uses through the webpack property
+				new gulpWebpack.webpack.optimize.DedupePlugin(),
+				new gulpWebpack.webpack.optimize.OccurenceOrderPlugin(true),
+				new gulpWebpack.webpack.optimize.UglifyJsPlugin({
+					minimize: true
+				})
+			]
+		})))
 
-		.pipe(gulp.dest(config.paths.dest));
+		.pipe(gulp.dest(config.paths.dest))
+		.pipe(notify({
+			message: pumped('JS Packaged & Minified!'),
+			onLast: true
+		}));
 };
