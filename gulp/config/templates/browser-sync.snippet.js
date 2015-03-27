b2ba729123a39df3e24f095dc18a7be774612a57
Max GJ Panas <m@maxpanas.com>
2015-03-27 17:03:00 +0200

    add note to browser-sync template to highlight the need to run gulp build before deploying

--- gulp/config/templates/browser-sync.snippet.js
+++ gulp/config/templates/browser-sync.snippet.js
@@ -6,10 +6,13 @@ module.exports = [
 	' * Browser-sync script loader',
 	' * to enable script/style injection',
 	' *',
+	' * Run "gulp build" to generate the theme',
+	' * for production before deploying!',
+	' *',
 	' */',
 	'add_action( \'wp_footer\', function () { ?>',
-		'\t<script type="text/javascript" id="__bs_script__">//<![CDATA[',
-			'\t\tdocument.write("<script async src=\'http://HOST:3000/browser-sync/browser-sync-client.js\'><\\/script>".replace("HOST", location.hostname));',
-		'\t//]]></script>',
+	'\t<script type="text/javascript" id="__bs_script__">//<![CDATA[',
+	'\t\tdocument.write("<script async src=\'http://HOST:3000/browser-sync/browser-sync-client.js\'><\\/script>".replace("HOST", location.hostname));',
+	'\t//]]></script>',
 	'<?php }, 999);'
 ].join('\n');
