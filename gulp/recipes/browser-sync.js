b708c31ee7182ac66b75ca811da4bc3fde2e4924
Max GJ Panas <m@maxpanas.com>
2015-04-22 12:45:03 +0300

    instruct browser-sync to ignore style.css changes

--- gulp/recipes/browser-sync.js
+++ gulp/recipes/browser-sync.js
@@ -8,7 +8,12 @@ var paths       = require('../config/common').paths;
  */
 module.exports = function () {
   browserSync({
-    files: [paths.theme.dest + '/**/*', '!' + paths.assets.dest + '/**/*.map'],
+    files: [
+			paths.theme.dest + '/**/*',
+			'!' + paths.assets.dest + '/**/*.map',
+			'!' + paths.theme.dest + '/README.md',
+			'!' + paths.theme.dest + '/style.css'
+		],
 		logSnippet: false,
 		server: false,
 		open: false
