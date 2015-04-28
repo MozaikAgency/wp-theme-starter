707c6e05f01ba95d23de061808f0927a0cd6c5f6
Max GJ Panas <m@maxpanas.com>
2015-04-28 16:57:14 +0300

    move browser-sync snippet to header so that it runs even if there are mid page errors - makes dev smoother

--- gulp/templates/browser-sync-snippet.js
+++ gulp/templates/browser-sync-snippet.js
@@ -10,7 +10,7 @@ module.exports = [
 	' * for production before deploying!',
 	' *',
 	' */',
-	'add_action( \'wp_footer\', function () { ?>',
+	'add_action( \'wp_head\', function () { ?>',
 	'\t<script type="text/javascript" id="__bs_script__">//<![CDATA[',
 	'\t\tdocument.write("<script async src=\'http://HOST:3000/browser-sync/browser-sync-client.js\'><\\/script>".replace("HOST", location.hostname));',
 	'\t//]]></script>',
