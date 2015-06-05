a31342895bc5a42d3f484812aacd8fa772c5f15f
Max GJ Panas <m@maxpanas.com>
2015-06-05 20:21:36 +0300

    add html5shiv in conditional comments into the header

--- theme/header.php
+++ theme/header.php
@@ -9,6 +9,10 @@
 <!doctype html>
 <html class="site" <?php language_attributes(); ?>>
 <head>
+	<!--[if lt IE 9]>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<![endif]-->
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
 	<meta name="viewport" content="width=device-width"/>
