63acafb6a600a250eef6218464563026f2ac81b1
Max GJ Panas <m@maxpanas.com>
2015-06-24 15:53:11 +0300

    fix docblock formatting

--- project.config.js
+++ project.config.js
@@ -2,16 +2,16 @@
  * Project info config object
  *
  * @params {
- * 	string      name         The built theme dirname and the theme text-domain
- * 	string      prettyName   The theme name as shown in the theme selector admin
- * 	string      description  A short description of the theme
+ *  string      name         The built theme dirname and the theme text-domain
+ *  string      prettyName   The theme name as shown in the theme selector admin
+ *  string      description  A short description of the theme
  *  bool|string parentTheme  (optional) If this is a child theme, then put the parent
  *                           theme's directory name here
- * 	string      version      The theme's version
- * 	string      author       The theme's author
- * 	string      authorURI    The theme's author URI
- * 	string      license      The theme's license
- * 	array       keywords     Keywords that could be associated with the theme
+ *  string      version      The theme's version
+ *  string      author       The theme's author
+ *  string      authorURI    The theme's author URI
+ *  string      license      The theme's license
+ *  array       keywords     Keywords that could be associated with the theme
  * }
  */
 module.exports = {
