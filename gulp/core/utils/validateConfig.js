9279f0bd20065b2e0f77dc7b2b2ca724f48970ce
Max GJ Panas <m@maxpanas.com>
2015-06-25 12:15:03 +0300

    fix project validation

--- gulp/core/utils/validateConfig.js
+++ gulp/core/utils/validateConfig.js
@@ -8,23 +8,46 @@ var path  = require('path');
  * @param project
  */
 module.exports = function (project) {
-
 	var validationFailed = false;
+
+
+	/**
+	 * Safely handle missing
+	 * project name or
+	 * project prettyName
+	 */
+	if (!project.name) {
+		validationFailed = true;
+
+		gutil.log('Config Error', gutil.colors.red('The \"name\" option in your project.config.js configuration cannot be empty'));
+	}
+	if (!project.prettyName) {
+		validationFailed = true;
+
+		gutil.log('Config Error', gutil.colors.red('The \"prettyName\" option in your project.config.js configuration cannot be empty'));
+	}
+
 
 	/**
 	 * Safely handle misconfigured
 	 * project name
 	 */
-	if (project.name === path.basename(__dirname)) {
+	var devThemeRoot = path.basename(path.resolve('./'));
+	if (project.name === devThemeRoot) {
 		validationFailed = true;
 
-		gutil.log('Config Error', gutil.colors.red('The \"name\" value in your project.config configuration \'' + project.name + '\''));
-		gutil.log('            ', gutil.colors.red('cannot be the same as the directory name of the development theme \'' + path.basename(__dirname) + '\'.'));
+		gutil.log('Config Error', gutil.colors.red('The \"name\" value in your project.config.js configuration \'' + project.name + '\''));
+		gutil.log('            ', gutil.colors.red('cannot be the same as the directory name of the development theme \'' + devThemeRoot + '\'.'));
 
 		gutil.log('Please either rename the development theme directory (to \'' + project.name + '_dev\' for example)');
-		gutil.log('or change the name value in your project.config to something else.');
+		gutil.log('or change the name value in your project.config.js to something else.');
 	}
 
+
+	/**
+	 * Exit the gulp process
+	 * if validation failed
+	 */
 	if (validationFailed) {
 		// if validation has failed
 		// do not continue further
