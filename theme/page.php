76eacce5fef649cdc9989e8aec6e1069cc43f358
Max GJ Panas <m@maxpanas.com>
2015-07-24 12:28:18 +0300

    respimg tests updated

--- theme/page.php
+++ theme/page.php
@@ -1,39 +1,143 @@
 <?php get_header(); ?>
 
-<div style="height: 2000px;"></div>
-<div style="height: 300px;position: relative;">
-	<?php
-	// using a background image
-	MOZ_RI::background( 11, 'medium', array(
-		'large'  => '(min-width: 500px)',
-		'full'   => '(min-width: 1200px)'
-	), array(
-		'lazy' => true
-	) );
-	?>
-</div>
+<article class="tests">
 
-<?php
+	<h1 class="tests__title">
+		Responsive Images Tests
+	</h1>
 
-MOZ_RI::picture( 10, 'medium', array(
-	'large' => '(min-width: 500px)',
-	'full'  => '(min-width: 1200px)'
-), array(
-	'lazy' => true
-) );
+	<dl class="tests__results">
+		<dt>Windows Chrome</dt> <dd>Works</dd>
+		<dt>Windows Firefox</dt><dd>Works</dd>
+		<dt>Windows Opera</dt>  <dd>Works</dd>
+		<dt>Windows Safari</dt> <dd>Object-fit broken</dd>
+		<dt>Windows IE 11</dt>  <dd>Object-fit broken</dd>
+	</dl>
 
-MOZ_RI::image( 8, array(
-	'full',
-	'large',
-	'medium'
-), array(
-	'(min-width: 1024px) 1024px',
-	'100vw'
-), array(
-	'lazy' => true
-) );
+	<section class="tests__section">
+		<h2 class="tests__section-title">
+			Basic Responsive Images
+		</h2>
+		<p class="tests__notice">
+			Images should load different sizes
+			at different viewport widths. Note
+			the differences between art directed
+			picture and img srcset-sizes
+		</p>
 
-?>
+		<p style="height: 300px;position: relative;">
+			<?php
+			// using a background image
+			MOZ_RI::background( 9, 'medium', array(
+				'large' => '(min-width: 500px)',
+				'full'  => '(min-width: 1200px)'
+			) );
+			?>
+		</p>
 
+		<p>
+			<?php
+			MOZ_RI::picture( 7, 'medium', array(
+				'large' => '(min-width: 500px)',
+				'full'  => '(min-width: 1200px)'
+			) );
+			?>
+		</p>
+
+		<p>
+			<?php
+			MOZ_RI::image( 6, array(
+				'full',
+				'large',
+				'medium'
+			), array(
+				'(min-width: 1024px) 1024px',
+				'100vw'
+			) );
+			?>
+		</p>
+	</section>
+
+	<section class="tests__section">
+		<h2 class="tests__section-title">
+			Responsive Images using object-fit
+		</h2>
+		<p class="tests__notice">
+			These are not background images. Images
+			should not look stretched
+		</p>
+
+		<p class="tests__object-fit">
+			<?php
+			MOZ_RI::picture( 5, 'medium', array(
+				'large' => '(min-width: 500px)',
+				'full'  => '(min-width: 1200px)'
+			) );
+			?>
+		</p>
+
+		<p class="tests__object-fit">
+			<?php
+			MOZ_RI::image( 4, array(
+				'full',
+				'large',
+				'medium'
+			), array(
+				'(min-width: 1024px) 1024px',
+				'100vw'
+			) );
+			?>
+		</p>
+	</section>
+
+	<section class="tests__section">
+		<h2 class="tests__section-title">
+			Lazy Loaded Responsive Images
+		</h2>
+		<p class="tests__notice">
+			Check the Network tab and note
+			when the pictures are requested
+		</p>
+
+		<p style="height: 300px;position: relative;">
+			<?php
+			// using a background image
+			MOZ_RI::background( 11, 'medium', array(
+				'large'  => '(min-width: 500px)',
+				'full'   => '(min-width: 1200px)'
+			), array(
+				'lazy' => true
+			) );
+			?>
+		</p>
+
+		<p>
+			<?php
+			MOZ_RI::picture( 10, 'medium', array(
+				'large' => '(min-width: 500px)',
+				'full'  => '(min-width: 1200px)'
+			), array(
+				'lazy' => true
+			) );
+			?>
+		</p>
+
+		<p>
+			<?php
+			MOZ_RI::image( 8, array(
+				'full',
+				'large',
+				'medium'
+			), array(
+				'(min-width: 1024px) 1024px',
+				'100vw'
+			), array(
+				'lazy' => true
+			) );
+			?>
+		</p>
+	</section>
+
+</article>
 
 <?php get_footer(); ?>
