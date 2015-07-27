335843fb57c0d4f03ba46efbf3d2260eae0627dd
Max GJ Panas <m@maxpanas.com>
2015-07-27 11:50:20 +0300

    update (lazy)respimage helpers to be more robust

--- theme/library/class-ri.php
+++ theme/library/class-ri.php
@@ -19,41 +19,33 @@ class MOZ_RI {
 	 * image should be
 	 * lazy-loaded
 	 *
-	 * @param array $extras
+	 * default: false
+	 *
+	 * @param array $flags
 	 *
 	 * @return bool
 	 */
-	public static function is_lazy( &$extras ) {
-		$lazy = false;
-
-		if ( isset( $extras['lazy'] ) ) {
-			$lazy = ! ! $extras['lazy'];
-			unset( $extras['lazy'] );
-		}
-
-		return $lazy;
+	public static function is_lazy( $flags ) {
+		return isset( $flags['lazy'] ) && ! ! $flags['lazy'];
 	}
 
 
 	/**
-	 * Determine whether an
-	 * image should have the
-	 * lazy class from the
+	 * Determine whether a
+	 * lazy image should have
+	 * the lazy class from the
 	 * start
 	 *
-	 * @param array $extras
+	 * default: true
+	 *
+	 * @param array $flags
 	 *
 	 * @return bool
 	 */
-	public static function should_add_lazy_class( &$extras ) {
-		$should_add = true;
-
-		if ( isset( $extras['lazy_class'] ) ) {
-			$should_add = ! ! $extras['lazy_class'];
-			unset( $extras['lazy_class'] );
-		}
-
-		return $should_add;
+	public static function should_add_lazy_class( $flags ) {
+		return isset( $flags['lazy_class'] )
+			? ! ! $flags['lazy_class']
+			: true;
 	}
 
 
@@ -63,15 +55,15 @@ class MOZ_RI {
 	 * whether it should
 	 * be lazy-loaded
 	 *
-	 * @param bool  $is_lazy
-	 * @param array $attrs
-	 * @param bool  $add_class
-	 * @param bool  $add_src
+	 * @param array     $flags
+	 * @param array     $attrs
+	 * @param bool|null $add_class
+	 * @param bool|null $add_src
 	 *
 	 * @return array
 	 */
-	public static function maybe_lazify( $is_lazy = false, $attrs = array(), $add_class = true, $add_src = true ) {
-		if ( ! $is_lazy ) {
+	public static function maybe_lazify( $flags, $attrs, $add_class = true, $add_src = true ) {
+		if ( ! self::is_lazy( $flags ) ) {
 			return $attrs;
 		}
 
@@ -83,7 +75,7 @@ class MOZ_RI {
 			}
 		}
 
-		if ( $add_class && self::should_add_lazy_class( $attrs ) ) {
+		if ( $add_class && self::should_add_lazy_class( $flags ) ) {
 			$attrs['class'] = isset( $attrs['class'] )
 				? "{$attrs['class']} lazyload"
 				: 'lazyload';
@@ -117,12 +109,13 @@ class MOZ_RI {
 	 *
 	 * @param int        $image
 	 * @param string     $base_size
-	 * @param array|null $sizes
-	 * @param array|null $extras
+	 * @param array      $sizes
+	 * @param array|null $attrs
+	 * @param array|null $flags
 	 *
 	 * @return bool|string
 	 */
-	public static function get_background( $image, $base_size, $sizes, $extras = array() ) {
+	public static function get_background( $image, $base_size, $sizes, $attrs = array(), $flags = array() ) {
 		if ( ! is_array( $sizes ) ) {
 			$sizes = array();
 		}
@@ -131,25 +124,23 @@ class MOZ_RI {
 			return false;
 		}
 
-		$is_lazy = self::is_lazy( $extras );
-
-		$attrs = array_merge( array(
+		$bgel_attrs = array_merge( array(
 			'class' => '',
 			'role'  => 'img',
 			'alt'   => self::get_img_alt( $image )
-		), $extras );
+		), (array) $attrs );
 
-		$alt = $attrs['alt'];
-		unset( $attrs['alt'] );
+		$alt = $bgel_attrs['alt'];
+		unset( $bgel_attrs['alt'] );
 
 		$content = MOZ_Html::get_element( 'span', array( 'class' => 'visuallyhidden' ), $alt );
 
-		$attrs['class'] .= ' moz-background-picture';
+		$bgel_attrs['class'] .= ' moz-background-picture';
 
-		if ( $is_lazy ) {
+		if ( self::is_lazy( $flags ) ) {
 			// lazy loaded background image
-			if ( self::should_add_lazy_class( $attrs ) ) {
-				$attrs['class'] .= ' lazyload';
+			if ( self::should_add_lazy_class( $flags ) ) {
+				$bgel_attrs['class'] .= ' lazyload';
 			}
 
 			$bgset = array( wp_get_attachment_image_src( $image, $base_size, false )[0] );
@@ -157,13 +148,13 @@ class MOZ_RI {
 				$src = wp_get_attachment_image_src( $image, $size, false );
 				$bgset[] = "$src[0] [$query]";
 			}
-			$attrs['data-sizes'] = 'auto';
-			$attrs['data-bgset'] = implode( ' | ', array_reverse( $bgset ) );
+			$bgel_attrs['data-sizes'] = 'auto';
+			$bgel_attrs['data-bgset'] = implode( ' | ', array_reverse( $bgset ) );
 
 		} else {
 			// not lazy-loaded background image
 			$unique = uniqid( 'moz-background-picture--' );
-			$attrs['class'] .= " $unique";
+			$bgel_attrs['class'] .= " $unique";
 
 			ob_start(); ?>
 
@@ -186,7 +177,7 @@ class MOZ_RI {
 			$content .= ob_get_clean();
 		}
 
-		return MOZ_Html::get_element( 'span', $attrs, $content );
+		return MOZ_Html::get_element( 'span', $bgel_attrs, $content );
 	}
 
 
@@ -197,11 +188,12 @@ class MOZ_RI {
 	 *
 	 * @param int        $image
 	 * @param string     $base_size
-	 * @param array|null $sizes
-	 * @param array|null $extras
+	 * @param array      $sizes
+	 * @param array|null $attrs
+	 * @param array|null $flags
 	 */
-	public static function background( $image, $base_size, $sizes, $extras = array() ) {
-		echo self::get_background( $image, $base_size, $sizes, $extras );
+	public static function background( $image, $base_size, $sizes, $attrs = array(), $flags = array() ) {
+		echo self::get_background( $image, $base_size, $sizes, $attrs, $flags );
 	}
 
 
@@ -211,17 +203,16 @@ class MOZ_RI {
 	 *
 	 * @param int        $image
 	 * @param string     $base_size
-	 * @param array|null $sizes
-	 * @param array|null $extras
+	 * @param array      $sizes
+	 * @param array|null $attrs
+	 * @param array|null $flags
 	 *
 	 * @return bool|string
 	 */
-	public static function get_picture( $image, $base_size, $sizes, $extras = array() ) {
+	public static function get_picture( $image, $base_size, $sizes, $attrs = array(), $flags = array() ) {
 		if ( ! wp_attachment_is_image( $image ) ) {
 			return false;
 		}
-
-		$is_lazy = self::is_lazy( $extras );
 
 		$content = array();
 
@@ -231,27 +222,27 @@ class MOZ_RI {
 		foreach ( array_reverse( $sizes ) as $size => $query ) {
 			$src = wp_get_attachment_image_src( $image, $size, false );
 
-			$attrs = self::maybe_lazify( $is_lazy, array(
+			$source_attrs = self::maybe_lazify( $flags, array(
 				'srcset' => esc_attr( $src[0] ),
 				'media'  => esc_attr( $query ),
 				'type'   => esc_attr( get_post_mime_type( $image ) )
 			), false, false );
 
-			$content[] = MOZ_Html::get_sc_element( 'source', $attrs );
+			$content[] = MOZ_Html::get_sc_element( 'source', $source_attrs );
 		}
 
 		$content[] = '<!--[if IE 9]></video><![endif]-->';
 
 		$base_src = wp_get_attachment_image_src( $image, $base_size, false );
 
-		$attrs = self::maybe_lazify( $is_lazy, array_merge( array(
+		$img_attrs = self::maybe_lazify( $flags, array_merge( array(
 			'srcset' => esc_attr( $base_src[0] ),
 			'alt'    => self::get_img_alt( $image )
-		), $extras ) );
+		), (array) $attrs ) );
 
-		$content[] = MOZ_Html::get_sc_element( 'img', $attrs );
+		$content[] = MOZ_Html::get_sc_element( 'img', $img_attrs );
 
-		return MOZ_Html::get_element( 'picture', array(), implode( '', $content ) );
+		return MOZ_Html::get_element( 'picture', null, implode( '', $content ) );
 	}
 
 
@@ -261,11 +252,12 @@ class MOZ_RI {
 	 *
 	 * @param int        $image
 	 * @param string     $base_size
-	 * @param array|null $sizes
-	 * @param array|null $extras
+	 * @param array      $sizes
+	 * @param array|null $attrs
+	 * @param array|null $flags
 	 */
-	public static function picture( $image, $base_size, $sizes, $extras = array() ) {
-		echo self::get_picture( $image, $base_size, $sizes, $extras );
+	public static function picture( $image, $base_size, $sizes, $attrs = array(), $flags = array() ) {
+		echo self::get_picture( $image, $base_size, $sizes, $attrs, $flags );
 	}
 
 
@@ -273,14 +265,15 @@ class MOZ_RI {
 	 * Get the markup for an image
 	 * using srcset and sizes
 	 *
-	 * @param int   $image
-	 * @param array $sources
-	 * @param array $sizes
-	 * @param array $extras
+	 * @param int        $image
+	 * @param array      $sources
+	 * @param array      $sizes
+	 * @param array|null $attrs
+	 * @param array|null $flags
 	 *
 	 * @returns string
 	 */
-	public static function get_image( $image, $sources, $sizes, $extras = array() ) {
+	public static function get_image( $image, $sources, $sizes, $attrs = array(), $flags = array() ) {
 		if ( ! is_array( $sizes ) ) {
 			$sizes = array();
 		}
@@ -300,15 +293,13 @@ class MOZ_RI {
 			return false;
 		}
 
-		$is_lazy = self::is_lazy( $extras );
-
-		$attrs = self::maybe_lazify( $is_lazy, array_merge( array(
+		$img_attrs = self::maybe_lazify( $flags, array_merge( array(
 			'srcset' => implode( ', ', $srcset ),
 			'sizes'  => implode( ', ', $sizes ),
 			'alt'    => self::get_img_alt( $image )
-		), $extras ) );
+		), (array) $attrs ) );
 
-		return MOZ_Html::get_sc_element( 'img', $attrs );
+		return MOZ_Html::get_sc_element( 'img', $img_attrs );
 	}
 
 
@@ -316,12 +307,13 @@ class MOZ_RI {
 	 * Print the markup for an image
 	 * using srcset and sizes
 	 *
-	 * @param int   $image
-	 * @param array $sources
-	 * @param array $sizes
-	 * @param array $extras
+	 * @param int        $image
+	 * @param array      $sources
+	 * @param array      $sizes
+	 * @param array|null $attrs
+	 * @param array|null $flags
 	 */
-	public static function image( $image, $sources, $sizes, $extras = array() ) {
-		echo self::get_image( $image, $sources, $sizes, $extras );
+	public static function image( $image, $sources, $sizes, $attrs = array(), $flags = array() ) {
+		echo self::get_image( $image, $sources, $sizes, $attrs, $flags );
 	}
 }
