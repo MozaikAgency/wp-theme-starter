a280d1a9b68b37a8e946e65dee2315febd93842a
Max GJ Panas <m@maxpanas.com>
2016-01-22 12:54:45 +0200

    make the theme more child theme friendly. now it is slightly easier to use wp-theme-starter as a parent theme.

--- theme/functions.php
+++ theme/functions.php
@@ -18,6 +18,10 @@ add_action( 'wp_enqueue_scripts', 'theme_styles' );
 
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
+// expose php variables to js. just uncomment line
+// below and see function theme_scripts_localize
+// add_action( 'wp_enqueue_scripts', 'theme_scripts_localize', 20 );
+
 /**--- Filters ---**/
 
 
@@ -34,78 +38,80 @@ add_action( 'wp_enqueue_scripts', 'theme_scripts' );
  *
  * @since 1.0
  */
-function theme_setup() {
+if ( ! function_exists( 'theme_setup' ) ) {
+	function theme_setup() {
 
-	// Let wp know we want to use html5 for content
-	add_theme_support( 'html5', array(
-		'comment-list',
-		'comment-form',
-		'search-form',
-		'gallery',
-		'caption'
-	) );
-
-
-	// Let wp know we want to use post thumbnails
-	/*
-	add_theme_support( 'post-thumbnails' );
-	*/
+		// Let wp know we want to use html5 for content
+		add_theme_support( 'html5', array(
+			'comment-list',
+			'comment-form',
+			'search-form',
+			'gallery',
+			'caption'
+		) );
 
 
-	// Register navigation menus for theme
-	/*
-	register_nav_menus( array(
-		'primary' => 'Main Menu',
-		'footer'  => 'Footer Menu'
-	) );
-	*/
+		// Let wp know we want to use post thumbnails
+		/*
+		add_theme_support( 'post-thumbnails' );
+		*/
 
 
-	// Let wp know we are going to handle styling galleries
-	/*
-	add_filter( 'use_default_gallery_style', '__return_false' );
-	*/
+		// Register navigation menus for theme
+		/*
+		register_nav_menus( array(
+			'primary' => 'Main Menu',
+			'footer'  => 'Footer Menu'
+		) );
+		*/
 
 
-	// Stop WP from printing emoji service on the front
-	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		// Let wp know we are going to handle styling galleries
+		/*
+		add_filter( 'use_default_gallery_style', '__return_false' );
+		*/
 
 
-	// Remove toolbar for all users in front end
-	show_admin_bar( false );
+		// Stop WP from printing emoji service on the front
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
 
-	// Add Custom Image Sizes
-	/*
-	add_image_size( 'ExampleImageSize', 1200, 450, true ); // Example Image Size
-	...
-	*/
+		// Remove toolbar for all users in front end
+		show_admin_bar( false );
 
 
-	// WPML configuration
-	// disable plugin from printing styles and js
-	// we are going to handle all that ourselves.
-	define( 'ICL_DONT_LOAD_NAVIGATION_CSS', true );
-	define( 'ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true );
-	define( 'ICL_DONT_LOAD_LANGUAGES_JS', true );
+		// Add Custom Image Sizes
+		/*
+		add_image_size( 'ExampleImageSize', 1200, 450, true ); // Example Image Size
+		...
+		*/
 
 
-	// Contact Form 7 Configuration needs to be done
-	// in wp-config.php. add the following snippet
-	// under the line:
-	// define( 'WP_DEBUG', false );
-	/*
-	//Contact Form 7 Plugin Configuration
-	define ( 'WPCF7_LOAD_JS',  false ); // Added to disable JS loading
-	define ( 'WPCF7_LOAD_CSS', false ); // Added to disable CSS loading
-	define ( 'WPCF7_AUTOP',    false ); // Added to disable adding <p> & <br> in form output
-	*/
+		// WPML configuration
+		// disable plugin from printing styles and js
+		// we are going to handle all that ourselves.
+		define( 'ICL_DONT_LOAD_NAVIGATION_CSS', true );
+		define( 'ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true );
+		define( 'ICL_DONT_LOAD_LANGUAGES_JS', true );
 
 
-	// Library Loader
-	get_template_part( 'library/library-loader' );
-	get_template_part( 'includes/includes-loader' );
+		// Contact Form 7 Configuration needs to be done
+		// in wp-config.php. add the following snippet
+		// under the line:
+		// define( 'WP_DEBUG', false );
+		/*
+		//Contact Form 7 Plugin Configuration
+		define ( 'WPCF7_LOAD_JS',  false ); // Added to disable JS loading
+		define ( 'WPCF7_LOAD_CSS', false ); // Added to disable CSS loading
+		define ( 'WPCF7_AUTOP',    false ); // Added to disable adding <p> & <br> in form output
+		*/
+
+
+		// Register Autoloaders Loader
+		include get_template_directory() . '/library/library-loader.php';
+		include get_template_directory() . '/includes/includes-loader.php';
+	}
 }
 
 
@@ -115,11 +121,12 @@ function theme_setup() {
  *
  * @since 1.0
  */
-function theme_styles() {
+if ( ! function_exists( 'theme_styles' ) ) {
+	function theme_styles() {
+		$theme_dir = get_stylesheet_directory_uri();
 
-	$theme_dir = get_template_directory_uri();
-
-	wp_enqueue_style( 'main', "$theme_dir/assets/css/main.css", array(), null, 'all' );
+		wp_enqueue_style( 'main', "$theme_dir/assets/css/main.css", array(), null, 'all' );
+	}
 }
 
 
@@ -129,9 +136,43 @@ function theme_styles() {
  *
  * @since 1.0
  */
-function theme_scripts() {
+if ( ! function_exists( 'theme_scripts' ) ) {
+	function theme_scripts() {
+		$theme_dir = get_stylesheet_directory_uri();
 
-	$theme_dir = get_template_directory_uri();
+		wp_enqueue_script( 'main', "$theme_dir/assets/js/main.js", array(), null, true );
+	}
+}
 
-	wp_enqueue_script( 'main', "$theme_dir/assets/js/main.js", array(), null, true );
+
+/**
+ * Attach variables we want
+ * to expose to our JS
+ *
+ * @since 3.12.0
+ */
+if ( ! function_exists( 'theme_scripts_localize' ) ) {
+	function theme_scripts_localize() {
+		$ajax_url_params = array();
+
+		// You can remove this block if you don't use WPML
+		if ( function_exists( 'wpml_object_id' ) ) {
+			/** @var $sitepress SitePress */
+			global $sitepress;
+
+			wp_localize_script( 'main', 'i18n', array(
+				'currentLang' => $sitepress->get_current_language()
+			) );
+
+			$ajax_url_params['lang'] = $sitepress->get_current_language();
+		}
+
+		$theme_dir = get_stylesheet_directory_uri();
+
+		wp_localize_script( 'main', 'urls', array(
+			'root'  => home_url(),
+			'theme' => $theme_dir,
+			'ajax'  => add_query_arg( $ajax_url_params, admin_url( 'admin-ajax.php' ) )
+		) );
+	}
 }
