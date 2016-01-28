5eb9e3d13287e6ba2c1c5bf1eb89e5b1bc4b4f9c
Max GJ Panas <m@maxpanas.com>
2016-01-28 18:59:06 +0200

    patch lazyQuire

--- gulp/core/utils/lazyQuire.js
+++ gulp/core/utils/lazyQuire.js
@@ -4,18 +4,17 @@
  * it is invoked and caches
  * it for future re-use
  *
- * @param require
  * @param path
  * @returns {Function}
  */
-module.exports = function lazyQuire(require, path) {
-	var worker;
+module.exports = function (require, path) {
+	var worker = '';
 
-	return function req() {
+	return function (a, b, c, d, e, f, g) {
 		if (!worker) {
 			worker = require(path);
 		}
 
-		return worker.apply(null, arguments);
+		return worker(a, b, c, d, e, f, g);
 	};
 };
