3103b05b44f9e8a9f29ac9bb8332b82858b169d6
Max GJ Panas <m@maxpanas.com>
2016-02-23 19:36:49 +0200

    remove object-fit polyfill because it no longer supports newer IE/Edge browsers

--- assets/js/core.js
+++ assets/js/core.js
@@ -15,25 +15,3 @@ import 'picturefill';
 /** Lazyload Images */
 import 'lazysizes/plugins/bgset/ls.bgset.js';
 import 'lazysizes';
-
-/** Object-fit/Object-position Polyfill */
-import objectFit from 'object-fit';
-
-window.objectFit = objectFit;
-
-const addEvent = () => window.addEventListener || window.attachEvent;
-const event = ( window.addEventListener ? '' : 'on' ) + 'DOMContentLoaded';
-
-addEvent()(event, () => {
-	objectFit.polyfill({
-		selector: '[data-object-fit="cover"]',
-		fittype: 'cover',
-		disableCrossDomain: true
-	});
-
-	objectFit.polyfill({
-		selector: '[data-object-fit="contain"]',
-		fittype: 'contain',
-		disableCrossDomain: true
-	});
-});
