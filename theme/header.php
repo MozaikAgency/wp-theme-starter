0ff491ab98add9be5142ba13d6dfc21378bee3b7
Max GJ Panas <m@maxpanas.com>
2016-02-26 15:23:40 +0200

    add tiny script to header to handle setting no-js/js class on the html element

--- theme/header.php
+++ theme/header.php
@@ -6,7 +6,7 @@
  */
 ?>
 <!doctype html>
-<html class="site" <?php language_attributes(); ?>>
+<html class="site no-js" <?php language_attributes(); ?>>
 <head>
 	<!--[if lt IE 9]>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -17,9 +17,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	
 	<title><?php wp_title(); ?></title>
-	
-	<?php wp_head(); ?>
+
+	<?php // replace the no-js class to js on the html element ?>
+	<script>document.documentElement.className=document.documentElement.className.replace(/\sno-js\s/,'js')</script>
+
+	<?php // load the core js polyfills ?>
 	<script async defer src="<?php echo get_template_directory_uri(); ?>/assets/js/core.js"></script>
+
+	<?php wp_head(); ?>
 </head>
 <body class="site__body">
 <?php // <body> closes in footer.php ?>
