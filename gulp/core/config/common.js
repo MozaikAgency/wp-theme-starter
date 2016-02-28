e780dcc2f5e45a974ddcf8e21c664e24b32a97df
Daniel Koop <mail@danielkoop.me>
2016-02-28 18:21:37 +0100

    [feature] Override the include path option. Useful for Vagrant

--- gulp/core/config/common.js
+++ gulp/core/config/common.js
@@ -12,6 +12,17 @@ var overrides = require('../../config/common');
  *
  */
 module.exports = deepMerge({
+	/**
+	 * Configure the following setting to replace the filepath which is
+	 * used in dev mode. The override_file_path.src is replaced with
+	 * override_file_path.dest. Useful when you run gulp on the local
+	 * machine and use vagrant for your dev machine.
+	 * 
+	 */
+	// 'override_file_path': {
+	// 	'src': '/the/build/path',
+	// 	'dest': '/the/include/path'
+	// },
 	paths: {
 		theme: {
 			src: 'theme',
