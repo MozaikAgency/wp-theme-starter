780695d2a96a323836a6d7a19c732dadd4170dac
Max GJ Panas <m@maxpanas.com>
2016-03-01 17:28:32 +0200

    add docblocks and extract nested functions

--- gulp/plugins/provideScripts.js
+++ gulp/plugins/provideScripts.js
@@ -3,49 +3,114 @@ var webpack   = require('webpack');
 // utils
 var deepMerge = require('../core/utils/deepMerge');
 
+
+/**
+ * Return a new object extended
+ * from the given object with the
+ * given key-value pair set on
+ * it
+ *
+ * @param obj
+ * @param key
+ * @param value
+ * @returns {*}
+ */
 function addKey(obj, key, value) {
 	var temp = {};
 	temp[key] = value;
 	return Object.assign({}, obj, temp);
 }
 
+
+/**
+ * Return an object mapping a local
+ * variable to a given package
+ *
+ * @param scripts
+ * @param script
+ * @returns {*}
+ */
 function setProvider(scripts, script) {
 	return script.local
 		? addKey(scripts, script.local, script.package)
 		: scripts;
 }
 
+
+/**
+ * Return an object mapping a package
+ * to a given global variable
+ *
+ * @param scripts
+ * @param script
+ * @returns {*}
+ */
 function setExternal(scripts, script) {
 	return script.global
 		? addKey(scripts, script.package, script.global)
 		: scripts;
 }
 
+
+/**
+ * Make sure the given script is
+ * valid
+ *
+ * @param script
+ * @returns {*|string|string|string|string|Array}
+ */
+function hasValidConfig(script) {
+	return script.package && (script.local || script.global);
+}
+
+
+/**
+ * Explode script configs with an
+ * array of local variables names
+ * to be able to map each variable
+ * to a package individually
+ *
+ * @param scripts
+ * @param script
+ * @returns {Array.<T>|string|*|{dist}}
+ */
+function extractNestedConfig(scripts, script) {
+	return scripts.concat(
+		Array.isArray(script.local)
+			? script.local.reduce(function (extracted, local) {
+					return extracted.concat({
+						local: local,
+						global: script.global,
+						package: script.package
+					});
+				}, [])
+			: script
+	);
+}
+
+
+/**
+ * Return a decorator for the scripts
+ * config object to add library script
+ * configuration
+ *
+ * @param scripts array
+ * @returns {Function}
+ */
 module.exports = function provideScripts(scripts) {
-	scripts = scripts
-		.filter(function (script) {
-			return script.package && (script.local || script.global);
-		}).reduce(function (arr, script) {
-			return arr.concat(
-				Array.isArray(script.local)
-					? script.local.reduce(function (agg, local) {
-							return agg.concat({
-								local: local,
-								global: script.global,
-								package: script.package
-							});
-						}, [])
-					: script
-			);
-		}, []);
+	scripts = scripts || [];
+
+	var validScripts = scripts
+		.filter(hasValidConfig)
+		.reduce(extractNestedConfig, []);
 
 	return function provideTo(config) {
 		config = config || {};
 
-		var externals = scripts.reduce(setExternal, {});
-		var providers = scripts.reduce(setProvider, {});
+		var externals = validScripts.reduce(setExternal, {});
+		var providers = validScripts.reduce(setProvider, {});
 
-		return scripts.length
+		return validScripts.length
 			? deepMerge({
 					options: {
 						webpack: {
